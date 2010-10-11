d88b2f19cc55757b9f64326d665520317056c6d2
dead.node@gmail.com <dead.node@gmail.com@ec273a5e-a2ee-11de-b1fe-bba1d6bec496>
2010-10-11 16:57:59 +0000

    firewall.sh: fixing syntax error.

--- trunk/firewall.sh
+++ trunk/firewall.sh
@@ -177,10 +177,11 @@ enable_blacklist() {
 					# look for cidr nets first, /32 included	
 					for net in $(egrep -v "^#" $file | egrep "/[0-9]{1}[0-9]{0,1}$"); do
 						$ipt -A BLACKLIST -d $ip -p ALL -s $net -j DROP 
-				done
+					done
 					# then single IP addresses
 					for ip in $(egrep -v "^#" $file | egrep -v "/[0-9]{1}[0-9]{0,1}$"); do
 						$ipt -A BLACKLIST -d $ip -p ALL -s $ip -j DROP
+					done
 				done
 			done
 		# an error has occoured
